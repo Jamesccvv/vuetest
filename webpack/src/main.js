c99cdcdc7fb3377b58c9195cda2e4175062b4200
James <jianchao_zhao@fabriciechina.com>
2020-04-29 14:40:57 +0800

    webpack 内存html

--- webpack/src/main.js
+++ webpack/src/main.js
@@ -3,15 +3,17 @@ import $ from 'jquery';
 
 $(function(){
     $('ul>li:odd').css('color','pink');
-    $('ul>li:even').css('background','pink');
-    $('ul>li:even').height('22px')
+    $('ul>li:even').css('background','red');
+    $('ul>li:even').height('66px')
 
 });
 
-// 1.使用webpack-dev-server 自动打包
+// 1.使用webpack-dev-server 自动打包   直接用 webpack 方式每次都要运行
 
 // 安装 npm i webpack-dev-server -D
 
 // 用法和使用webpack 一样  非全局安装  "dev": "webpack-dev-server"  运行npm run dev  即可  http://localhost:8080/ 目前直接托管到内存中了bundle.js
 
-// --open 代表打开浏览器 --port 3000 端口  --contentBase src 可以直接打开这个目录 --hot 热更新
+// --open 代表打开浏览器 --port 3000 端口  --contentBase src 可以直接打开这个目录 --hot 热更新追加方式
+
+// npm i html-webpack-plugin  页面缓存插件
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,8 +1,30 @@
 const path = require('path');
+
+// 启用自动打包的第二种方式
+const webpack = require('webpack');
+
+// 内存中生成HTML 插件
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: path.join(__dirname, './src/main.js'),//入口
     output: {
         path: path.join(__dirname, './dist'),
         filename: 'bundle.js'
-    }
+    },
+    devServer: {
+        open: true,
+        port: 3000,
+        hot: true,
+        contentBase:'src'
+    },
+    plugins:[
+        new webpack.HotModuleReplacementPlugin(),
+
+        new htmlWebpackPlugin({
+            template:path.join(__dirname,'index.html'),// 指定
+            //filename:'index1.html' // 生成内存中的
+            filename:'index.html' // 生成内存中的
+        })
+    ]
 };
