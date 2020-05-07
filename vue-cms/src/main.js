2dfdfd156256b66a89c8d8c8af11343c6030b018
James <jianchao_zhao@fabriciechina.com>
2020-05-07 09:31:26 +0800

    初始化界面

---
+++ vue-cms/src/main.js
@@ -0,0 +1,16 @@
+// 入口文件
+
+import Vue from 'vue'
+
+import App from './App.vue'
+
+import { Header } from 'mint-ui';
+
+import '../lib/mui/css/mui.css';
+Vue.component(Header.name, Header);
+
+
+var vm = new Vue({
+    el: '#app',
+    render: c => c(App)
+});
---
+++ vue-cms/src/router.js
@@ -0,0 +1,18 @@
+
+import VueRouter from 'vue-router'
+
+var router = new VueRouter({
+    routes: [
+        // {
+        //     path: '/account', component: account,
+        //     children: [
+        //         {path: 'subLogin', component: subLogin},
+        //         {path: 'subRegister', component: subRegister}
+        //     ]
+        // },
+        // {path: '/goodsList', component: goodsList}
+    ]
+});
+
+// es6 语法暴露对象
+export default router
